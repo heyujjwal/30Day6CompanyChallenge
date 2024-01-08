2fef50fc6e3d9d72125c3c3cea519c8a74076987
Ujjwal Gupta <107796383+heyujjwal@users.noreply.github.com>
2024-01-08 23:35:31 +0530

    Create Circle and Rectangle Overlapping.cpp

---
+++ Microsoft/Circle and Rectangle Overlapping.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int nearestX=max(x1,min(x2,xCenter));
+        int nearestY=max(y1,min(y2,yCenter));
+        int distX=nearestX-xCenter;
+        int distY=nearestY-yCenter;
+        return (distX*distX)+(distY*distY)<=(radius*radius);
+    }
+};
