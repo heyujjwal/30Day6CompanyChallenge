c2212f38623dd6e348b883e133bc1b881bb0cf0c
Ujjwal Gupta <107796383+heyujjwal@users.noreply.github.com>
2024-01-10 00:39:40 +0530

    Create Find the City With the Smallest Number of Neighbors at a Threshold Distance.cpp

---
+++ Microsoft/Find the City With the Smallest Number of Neighbors at a Threshold Distance.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n,vector<int>(n,INT_MAX));
+        for(auto it:edges){
+            dist[it[0]][it[1]]=it[2];
+            dist[it[1]][it[0]]=it[2];
+        }
+        for(int i=0;i<n;i++) dist[i][i]=0;
+        for(int k=0;k<n;k++)
+        {
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][k]==INT_MAX || dist[k][j]==INT_MAX)
+                    continue;
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+        int CnCt=n;
+        int city=-1;
+        for(int c=0;c<n;c++){
+            int cnt=0;
+            for(int adjc=0;adjc<n;adjc++){
+                if(dist[c][adjc]<=distanceThreshold)
+                cnt++;
+            }
+
+            if(cnt<=CnCt)
+           {
+                CnCt=cnt;
+            city=c;
+           }
+        }
+        return city;
+    }
+};
