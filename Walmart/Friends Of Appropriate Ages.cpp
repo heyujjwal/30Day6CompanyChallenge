87797d1d4c1ac5dd0bffb037693a436a130c23bb
Ujjwal Gupta <107796383+heyujjwal@users.noreply.github.com>
2024-01-14 23:55:38 +0530

    Create Friends Of Appropriate Ages.cpp

---
+++ Walmart/Friends Of Appropriate Ages.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        sort(ages.begin(),ages.end());
+        int ans = 0;
+        // 2 check 
+        // 1. ->  y > x/2 + 7 ;
+        // 2. ->  y <= x;
+        for(int i=0;i<ages.size();i++){
+            int x = ages[i];
+            int y = x/2 + 7;
+			// condition check
+            if(y >= x) continue;
+            auto it = upper_bound(ages.begin() , ages.end() , y) - ages.begin();
+            auto it2 = upper_bound(ages.begin(), ages.end() , x) - ages.begin();
+            if(it < i){ans += (i - it);}
+            if(it2 > i) ans += (it2 - i - 1);
+        }
+        return ans;
+    }
+};
